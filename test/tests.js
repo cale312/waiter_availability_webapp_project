08ec20159a8c68e4121cdbe824d0c94a080a74ae
cale312 <zikisacalesogaxa@gmail.com>
2017-05-12 15:23:17 +0200

    test folder for all the tests

---
+++ test/tests.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const waiterLogin = require('../controllers/waiterLoginController');
+
+describe('All tests for my function', function(done) {
+
+  it('should login the right user', function() {
+    assert.equals();
+  });
+
+});
