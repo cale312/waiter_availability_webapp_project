5f35fda87d4c809d3f2a5cc6608a117c1de0b508
cale312 <zikisacalesogaxa@gmail.com>
2017-05-12 16:44:59 +0200

    adde the app get and post commands

--- controllers/waiterLoginController.js
+++ controllers/waiterLoginController.js
@@ -1,3 +1,9 @@
 module.exports = function(app) {
-  
+  app.get('/login', function(req, res) {
+    res.render('waiterLogin', {});
+  });
+
+  app.post('/login', function(req, res) {
+    res.render('waiterLogin', {});
+  });
 }
