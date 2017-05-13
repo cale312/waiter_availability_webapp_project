007d756296e07eddb3eaffde575d1ebd8f69275c
cale312 <zikisacalesogaxa@gmail.com>
2017-05-13 17:40:48 +0200

    required all the modules to be used by the app

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ var express = require('express');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 var waiterLogin = require('./controllers/waiterLoginController');
+var registration = require('./controllers/registrationController');
 var connectDB = require('./models/connection');
 var app = express();
 
-waiterLogin(app);
-connectDB(app)
 
 app.use('/public', express.static('public'));
 app.use(bodyParser.urlencoded({
@@ -22,6 +21,10 @@ app.set('view engine', 'handlebars');
 app.get('/', function(req, res) {
   res.render('index', {})
 });
+
+connectDB(app)
+waiterLogin(app);
+registration(app);
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
