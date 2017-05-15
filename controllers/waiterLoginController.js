625ec48bad1ac7c7ef7e3ce6fbd0dbd2493306f3
cale312 <zikisacalesogaxa@gmail.com>
2017-05-15 14:53:36 +0200

    controller for the login of the waiters

--- controllers/waiterLoginController.js
+++ controllers/waiterLoginController.js
@@ -1,9 +1,44 @@
 module.exports = function(app) {
+  const mongoose = require('mongoose');
+  const waiter = require('../models/model');
+
+  function loginController(username, password, fn) {
+    waiter.findOne({
+        waiter: username,
+      },
+      function(err, exists) {
+        if (exists) {
+          return;
+        } else {
+          return;
+        }
+      });
+  }
+
   app.get('/login', function(req, res) {
     res.render('waiterLogin', {});
   });
 
   app.post('/login', function(req, res) {
-    res.render('waiterLogin', {});
+    var username = req.body.username;
+    var password = req.body.password;
+    var login = req.body.login;
+    var register = req.body.signup;
+
+    if (login) {
+      waiter.findOne({
+        waiter: username
+      }, function(err, waiter) {
+        if (err) {
+          return next(err);
+        } else if (waiter && password === waiter.password) {
+          res.render('waiters', {});
+        } else {
+          res.render('waiterLogin', {error: 'wrong password/username'});
+        }
+      });
+    } else if (register) {
+      res.render('signup', {});
+    }
   });
 }
