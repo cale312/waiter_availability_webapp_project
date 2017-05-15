8e0e148be844bdcbc51753a7773473b5ef2724c5
cale312 <zikisacalesogaxa@gmail.com>
2017-05-15 14:48:26 +0200

    file to store the models for the database

---
+++ models/model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+var waiterSchema = mongoose.Schema({
+  waiter: String,
+  password: String,
+  email: String,
+  Monday: Boolean,
+  Tuesday: Boolean,
+  Wednesday: Boolean,
+  Thursday: Boolean,
+  Friday: Boolean,
+  Saturday: Boolean,
+  Sunday: Boolean
+});
+const waiter = mongoose.model('waiter', waiterSchema);
+
+module.exports = waiter;
