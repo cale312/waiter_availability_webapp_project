d32eeef2288cd899ffc74ae0e249defef465e93c
cale312 <zikisacalesogaxa@gmail.com>
2017-05-16 17:44:45 +0200

    added the waiter information controller to the server

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 var waiterLogin = require('./controllers/waiterLoginController');
 var registration = require('./controllers/registrationController');
+var waiterInfo = require('./controllers/waiterInfoController');
 var connectDB = require('./models/connection');
 var app = express();
 
@@ -22,9 +23,12 @@ app.get('/', function(req, res) {
   res.render('index', {})
 });
 
+var name = [];
+
 connectDB(app)
 waiterLogin(app);
 registration(app);
+waiterInfo(app);
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
