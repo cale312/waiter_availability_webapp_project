500326f5a4182710e0d8cac8524375d52b90c1c2
cale312 <zikisacalesogaxa@gmail.com>
2017-05-16 17:46:14 +0200

    controller for the info/days the waiter selected he/she will be available in

--- controllers/waiterInfoController.js
+++ controllers/waiterInfoController.js
@@ -2,5 +2,12 @@ module.exports = function(app) {
   const mongoose = require('mongoose');
   const waiter = require('../models/model');
 
+  app.get('/waiters', function(req, res) {
+    res.render('waiters', {});
+  });
+
+  app.post('/waiters', function(req, res) {
+    res.render('waiters', {});
+  });
 
 }
