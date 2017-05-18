300e0fc30d98e30ead5ecfac798b0a1829c81523
cale312 <zikisacalesogaxa@gmail.com>
2017-05-18 17:47:11 +0200

    added a function that controls the login to login the right user

--- controllers/waiterLoginController.js
+++ controllers/waiterLoginController.js
@@ -2,42 +2,53 @@ module.exports = function(app) {
   const mongoose = require('mongoose');
   const waiter = require('../models/model');
 
-  function loginController(username, password, fn) {
+  function manageLogin(username, password, fn) {
     waiter.findOne({
-        waiter: username,
-      },
-      function(err, exists) {
-        if (exists) {
-          return;
-        } else {
-          return;
-        }
-      });
+      waiter: username
+    }, function(err, waiterExists) {
+      if (err){
+        return err;
+      }
+
+      if (waiterExists && password === waiterExists.password) {
+        fn(null, {status : 'LoggedIn'});
+      } else {
+        fn(null, {status : 'NotLoggedIn'});
+      }
+
+    });
   }
 
   app.get('/login', function(req, res) {
-    res.render('waiterLogin', {});
+    res.render('login', {});
   });
 
-  app.post('/login', function(req, res) {
+
+  app.get('/shifts/:username', function(req, res, next) {
+    res.render('shifts', {});
+  });
+
+  app.get('/shifts', function(req, res, next) {
+    res.render('shifts', {});
+  });
+
+
+  app.post('/login', function(req, res, next) {
     var username = req.body.username;
     var password = req.body.password;
     var login = req.body.login;
     var register = req.body.signup;
 
     if (login) {
-      waiter.findOne({
-        waiter: username
-      }, function(err, waiter) {
+      manageLogin(username, password, function(err, result) {
         if (err) {
           return next(err);
-        } else if (waiter && password === waiter.password) {
-          name = username;
-          res.render('waiters', {username: name});
-        } else if (username === 'admin' && password === 'admin') {
-          res.render('admin', {admin: name});
         } else {
-          res.render('waiterLogin', {error: 'wrong password/username'});
+          if (result.status === 'LoggedIn') {
+            res.redirect('/shifts');
+          } else {
+            res.redirect('/login');
+          }
         }
       });
     } else if (register) {
