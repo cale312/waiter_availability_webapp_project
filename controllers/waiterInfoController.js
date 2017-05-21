dd86f856ae50a5d9570a0e6d4dc90483aab4aa39
cale312 <zikisacalesogaxa@gmail.com>
2017-05-21 05:46:15 +0200

    script to stop any username from logging in using parameters

--- controllers/waiterInfoController.js
+++ controllers/waiterInfoController.js
@@ -1,20 +1,32 @@
+'use strict';
 module.exports = function(app) {
   const waiter = require('../models/shiftsModel');
 
+  function manageInfo(waiterName, day, fn) {
+
+  }
+
   app.get('/shifts/:id', function(req, res, next) {
     const waiterName = req.params.id;
-    res.render('shifts');
+    waiter.findOne({
+      waiter: waiterName
+    }, function (err, result) {
+      if (err) {
+        return err;
+      } else if (result) {
+        res.render('shifts');
+      } else {
+        res.redirect('/login');
+      }
+    });
   });
 
-  app.post('/shifts', function(req, res, next) {
+  app.post('/shifts/:id', function(req, res, next) {
+    const waiterName = req.params.id;
     var submit = req.body.submit;
     var day = req.body.day;
-    // console.log(req.body);
+    res.redirect('/shifts/' + waiterName);
 
-    res.render('shifts');
   });
 
-  // app.get('/admin', function(req, res, next) {
-  //   res.render('admin');
-  // });
 }
