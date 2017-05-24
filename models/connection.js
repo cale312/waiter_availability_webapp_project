9734b7bbb54e7fe7277ab3242f0f36621d531c90
cale312 <zikisacalesogaxa@gmail.com>
2017-05-24 13:42:22 +0200

    module for the connection to the database

--- models/connection.js
+++ models/connection.js
@@ -3,6 +3,8 @@ module.exports = function(app) {
   const mongoURL = process.env.MONGO_DB_URL || "mongodb://localhost/waiters";
   mongoose.connect(mongoURL);
 
+  mongoose.Promise = global.Promise;
+
   var db = mongoose.connection;
 
   db.on('error', console.error.bind(console, 'connection error:'));
