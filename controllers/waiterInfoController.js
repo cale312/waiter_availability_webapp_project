724980e44ebd93195d76aed2c2408a88ebdc7e25
cale312 <zikisacalesogaxa@gmail.com>
2017-05-26 14:33:23 +0200

    script for persisting the checked boxes after log in or submit

--- controllers/waiterInfoController.js
+++ controllers/waiterInfoController.js
@@ -1,15 +1,15 @@
-// 'use strict';
+'use strict';
 module.exports = function(app) {
   const waiter = require('../models/shiftsModel');
 
   // Function that finds and updates the shifts selected by the waiter...
-  function manageInfo(waiterName, days, fn){
+  function manageInfo(waiterName, days, fn) {
     waiter.findOne({
       waiter: waiterName
-    }, function(err, result){
-      if (err){
+    }, function(err, result) {
+      if (err) {
         return err;
-      } else  if (result) {
+      } else if (result) {
         result.shifts = days;
         result.save();
         fn(null, {
@@ -23,19 +23,67 @@ module.exports = function(app) {
     });
   }
 
-  app.get('/shifts/:id', function(req, res, next) {
-    const waiterName = req.params.id;
+  // Finds the waiter in the database and returns the days array
+  function daysChecked(waiterName, fn) {
     waiter.findOne({
       waiter: waiterName
     }, function(err, result) {
       if (err) {
         return err;
-      } else if (result) {
-        res.render('shifts', {username: waiterName, shifts: result.shifts});
-      } else {
-        res.redirect('/login');
+      }
+
+      if (result) {
+        fn(null, result.shifts)
       }
     });
+  }
+
+  app.get('/shifts/:id', function(req, res, next) {
+    const waiterName = req.params.id;
+
+
+    daysChecked(waiterName, function(err, result) {
+
+      // Changes the returned array of days to an Object and returns the new Object
+      function arrToobj(daysArr) {
+        var daysObj = {};
+        var daysArray = result.days;
+        for (let i = 0; i < daysArr.length; i++){
+          daysObj[daysArr[i]] = 'checked';
+        }
+        return daysObj;
+      }
+
+      if (err) {
+        return next(err);
+      } else {
+        waiter.findOne({
+          waiter: waiterName
+        }, function(err, result) {
+          if (err) {
+            return err;
+          } else if (result) {
+            daysChecked(waiterName, function(err, checkedDays){
+              if (err) {
+                return err;
+              } else {
+                // console.log(checkedDays);
+                let checkedDaysFixed = arrToobj(checkedDays);
+                // console.log(checkedDaysFixed);
+                res.render('shifts', {
+                  username: waiterName,
+                  shifts: result.shifts,
+                  days: checkedDaysFixed
+                });
+              }
+            })
+          } else {
+            res.redirect('/login');
+          }
+        });
+      }
+    })
+
   });
 
 
@@ -44,11 +92,10 @@ module.exports = function(app) {
     var submit = req.body.submit;
     var days = req.body.day;
 
-    manageInfo(waiterName, days, function(err, result){
-      if (err){
+    manageInfo(waiterName, days, function(err, result) {
+      if (err) {
         return next(err);
-      } else if (result.status === 'updated'){
-        console.log('waiter shift updated');
+      } else if (result.status === 'updated') {
         res.redirect('/shifts/' + waiterName);
       } else {
         console.log('fail');
