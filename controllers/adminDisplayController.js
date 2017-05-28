128bc00e70c1628ff89fd97fc10731fc1fb6e0f3
cale312 <zikisacalesogaxa@gmail.com>
2017-05-28 02:05:37 +0200

    retrieved all the waiters from the database and grouped them according to their available days, from monday to sunday

--- controllers/adminDisplayController.js
+++ controllers/adminDisplayController.js
@@ -1,10 +1,70 @@
 'use strict';
-module.exports = function(app){
-  const shifts = require('../models/shiftsModel');
+module.exports = function(app) {
+  const waiters = require('../models/shiftsModel');
 
-  app.get('/admin/:id', function(req, res){
-    var admin = req.params.id;
-    res.render('admin');
+  // Function that returns all the waiters in teh database
+  function getWaiters(fn) {
+    waiters.find({}, function(err, result) {
+      if (err) {
+        return err;
+      } else if (result) {
+        fn(null, result);
+      }
+    });
+  }
+
+  // Should return an object of days with waiters paored to the days they are available in...
+  // Have to return something like this... {monday: ['cale oasin'], tuesday: ['ted mosby'] }
+
+  app.get('/admin/:id', function(req, res, next) {
+    const admin = req.params.id;
+    var monday = [];
+    var tuesday = [];
+    var wednesday = [];
+    var thursday = [];
+    var friday = [];
+    var saturday = [];
+    var sunday = [];
+
+    var daysObj = {
+      monday,
+      tuesday,
+      wednesday,
+      thursday,
+      friday,
+      saturday,
+      sunday
+    }
+
+    getWaiters(function(err, result) {
+      for (let i = 0; i < result.length; i++) {
+        var resultShifts = result[i].shifts;
+        var waiter = result[i].waiter;
+        // console.log(result[i]);
+        for (let i = 0; i < resultShifts.length; i++) {
+          // console.log(resultShifts[i])
+          if (err) {
+            return err;
+          } else if (resultShifts[i] === 'Monday') {
+            monday.push(waiter);
+          } else if (resultShifts[i] === 'Tuesday') {
+            tuesday.push(waiter);
+          } else if (resultShifts[i] === 'Wednesday') {
+            wednesday.push(waiter);
+          } else if (resultShifts[i] === 'Thursday') {
+            thursday.push(waiter);
+          } else if (resultShifts[i] === 'Friday') {
+            friday.push(waiter);
+          } else if (resultShifts[i] === 'Saturday') {
+            saturday.push(waiter);
+          } else if (resultShifts[i] === 'Sunday') {
+            sunday.push(waiter);
+          }
+        }
+      }
+      res.render('admin', daysObj);
+    });
+    // console.log(daysObj);
   });
 
 }
